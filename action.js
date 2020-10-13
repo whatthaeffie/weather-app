fb07d84f2146ff7c667407bcacd16472912844a7
whatthaeffie <aprilefff@gmail.com>
2020-10-12 22:54:54 -0400

    added project to folder

---
+++ action.js
@@ -0,0 +1,49 @@
+let now = new Date();
+let showDay = document.querySelector("#date");
+let hours = now.getHours();
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+if (hours < 10) {
+  hours = `0${minutes}`;
+}
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+showDay.innerHTML = `${day} ${hours}:${minutes}`;
+//
+function showCity(event) {
+  event.preventDefault();
+  let newInput = document.querySelector("#city");
+  let answer = document.querySelector("#answer");
+  newInput.innerHTML = answer.value;
+}
+let form = document.querySelector("#searching");
+form.addEventListener("submit", showCity);
+
+//
+function convertToFahrenheit(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 66;
+}
+
+function convertToCelsius(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = 19;
+}
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", convertToFahrenheit);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", convertToCelsius);
